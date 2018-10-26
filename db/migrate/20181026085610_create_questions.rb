36b11bacd42079bcc9187948fb39718c348fd854
yuneszadi <yuneszadi@gmail.com>
2018-10-26 16:32:55 +0300

    add tests for models

--- db/migrate/20181026085610_create_questions.rb
+++ db/migrate/20181026085610_create_questions.rb
@@ -3,6 +3,7 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
     create_table :questions do |t|
       t.string :title
       t.text :body
+      t.references :test, foreign_key: true
 
       t.timestamps
     end
--- spec/models/test_spec.rb
+++ spec/models/test_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Test, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:questions).dependent(:destroy) }
+
+  it { should validate_presence_of :title}
 end
