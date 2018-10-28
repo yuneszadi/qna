c7a4284ebcf69b040b2637667096701fe560937c
yuneszadi <yuneszadi@gmail.com>
2018-10-28 20:29:55 +0200

    tests for Answerss controller

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :answer do
-    title { "MyString" }
+    question_id { 1 }
     body { "MyText" }
+  end
+
+  factory :invalid_answer, class: Answer do
+    question_id { nil }
+    body { nil }
   end
 end
