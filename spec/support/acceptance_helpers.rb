1dd50a043e1d25ce2a55c452537c3b71a4503121
yuneszadi <yuneszadi@gmail.com>
2018-10-30 21:17:44 +0200

    add acceptance tests for sign up

--- spec/support/acceptance_helpers.rb
+++ spec/support/acceptance_helpers.rb
@@ -5,4 +5,12 @@ module AcceptanceHeplers
     fill_in 'Password', with: user.password
     click_on 'Log in'
   end
+
+  def sign_up
+    visit new_user_registration_path
+    fill_in 'Email', with: 'user@test.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+  end
 end
