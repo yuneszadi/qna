6d2f7e066ef2049f0da568eb74c31a20e49247db
yuneszadi <yuneszadi@gmail.com>
2018-10-30 20:59:27 +0200

    add acceptance tests for sign out

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper.rb'
+ feature 'User sign out', %q{
+  In order to exit
+  as an authorized user,
+  I want to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+   scenario 'Authintacated user try to sign out' do
+    sign_in(user)
+    click_on 'Log out'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
