a60dc32eb000d02180f8452c65bb04b6469007a4
yuneszadi <yuneszadi@gmail.com>
2018-11-19 21:59:44 +0200

    Questions ActionCable

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
 
   before_action :authenticate_user!, only: [ :update, :new, :create, :update ]
   before_action :find_question, only: [ :show, :edit, :update, :destroy ]
+  after_action :publish_question, only: :create
 
   def index
     @questions = Question.all
@@ -48,5 +49,12 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:title, :body, attachments_attributes: [:file, :destroy])
+  end
+
+  def publish_question
+    return if @question.errors.any?
+    ActionCable.server.broadcast 'questions', ApplicationController.render(
+      partial: 'questions/collection_item', locals: { question: @question }, layout: false
+    )
   end
 end
--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -34,4 +34,30 @@ feature 'Create question', %q{
     click_on 'Create'
     expect(page).to have_content "Title can't be blank"
   end
+
+  context 'multiple sessions' do
+    scenario 'question appears on another user\'s page', js: true do
+      Capybara.using_session('user') do
+        sign_in(user)
+        visit questions_path
+      end
+
+      Capybara.using_session('guest') do
+        visit questions_path
+      end
+
+      Capybara.using_session('user') do
+        click_on 'Ask question'
+        fill_in 'Title', with: 'Test question'
+        fill_in 'Body', with: 'text text'
+        click_on 'Create'
+        expect(page).to have_content 'Test question'
+        expect(page).to have_content 'text text'
+      end
+
+      Capybara.using_session('guest') do
+        expect(page).to have_content 'Test question'
+      end
+    end
+  end
 end
