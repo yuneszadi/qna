778d679fe9ad400e2a0e6a6ae49752821eaedbdc
yuneszadi <yuneszadi@gmail.com>
2018-12-08 18:09:19 +0200

    add reputation

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  subject { build(:question) }
+
   it { should have_many(:answers).dependent(:destroy) }
   it { should belong_to(:user) }
   it { should have_many(:attachments) }
@@ -9,4 +11,32 @@ RSpec.describe Question, type: :model do
   it { should validate_presence_of :body}
 
   it { should accept_nested_attributes_for :attachments }
+
+  context "reputation" do
+    let(:user) { create(:user) }
+    subject { build(:question, user: user) }
+
+    it 'should calculate reputation after create' do
+      expect(Reputation).to receive(:calculate).with(subject)
+      subject.save!
+    end
+
+    it 'should not calculate reputation after create' do
+      subject.save!
+      expect(Reputation).to_not receive(:calculate)
+      subject.update(title: 'New title')
+    end
+
+    it 'should save user reputation' do
+      allow(Reputation).to receive(:calculate).and_return(5)
+      expect { subject.save! }.to change(user, :reputation).by(5)
+    end
+    
+    it 'test time' do
+      now = Time.now.utc
+      allow(Time).to receive(:now) { now }
+      subject.save!
+      expect(subject.created_at).to eq now
+    end
+  end
 end
