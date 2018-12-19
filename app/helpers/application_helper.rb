813a95f2b92c96917bde4d0451227613b4e71d32
yuneszadi <yuneszadi@gmail.com>
2018-12-19 21:39:41 +0200

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,6 @@
 module ApplicationHelper
   def render_result(object)
-    type = object.class.to_s
-
-    case type
-    when "Question"
-       render(partial: 'search/question', locals: { question: object } )
-    when "Answer"
-       render(partial: 'search/answer', locals: { answer: object } )
-    when "User"
-       render(partial: 'search/user', locals: { user: object } )
-    when "Comment"
-      if object.commentable_type == "Question"
-        render(partial: 'search/question_comment', locals: { comment: object } )
-      elsif object.commentable_type == "Answer"
-        render(partial: 'search/answer_comment', locals: { comment: object } )
-      end
-    end
+    type = object.class.to_s.downcase
+    render(partial: 'search/#{type}' )
   end
 end
